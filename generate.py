8be051876850d3ac37d07933acaa6ef98cd0204d
Abhinav Anand <abhinav_anand@live.com>
2019-11-03 21:49:21 +0530

    Improve generator script

--- generate.py
+++ generate.py
@@ -1,14 +1,12 @@
 from sklearn.datasets import make_spd_matrix
 import random
 
-n = 1000
+n = int(input())
 x = make_spd_matrix(n)
 
-with open('sample.txt', 'w') as fp:
-    fp.write(f"{n} {n}\n")
-    for row in x:
-        for elem in row:
-            fp.write(str(elem))
-            fp.write(" ")
-    fp.write(str(random.randint(0, 100)))
-    fp.write("\n")
+print(f"{n} {n}")
+
+for row in x:
+    for elem in row:
+        print("{0:.4f}".format(elem), end=' ')
+    print((str(random.randint(0, 100))))
